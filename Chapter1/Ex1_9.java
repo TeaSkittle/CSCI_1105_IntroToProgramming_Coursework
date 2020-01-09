6ef7e8508ecf4cb4304c2d0bb8b29d59fdb9337a
404kevnotfound <student@HSiMac4-141.local>
2020-01-09 11:22:40 -0700

    Add Ex1_9.java

---
+++ Chapter1/Ex1_9.java
@@ -0,0 +1,15 @@
+/*
+Author: Travis Dowd
+Date: 1-9-2020
+
+Chapter 1, Exercise 1-9
+ */
+
+class Ex1_9 {
+    public static void main(String[] args) {
+	double width = 4.5;
+	double height = 7.9;
+	System.out.print("Area: " + (width * height)+ "\n");
+	System.out.print("Perimeter: " + ((width + height) * 2) + "\n");
+    }
+}
