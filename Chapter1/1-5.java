fdd16fc956a5588fdc01f6141732780b1edd6fd5
404kevnotfound <student@HSiMac4-141.local>
2020-01-09 10:37:45 -0700

    Add 1-5.java, edit .gitignore with emacs section

---
+++ Chapter1/1-5.java
@@ -0,0 +1,12 @@
+/* 
+Author: Travis Dowd
+Date: 1-9-2020
+
+Ch1. Exercisze 1-5
+ */
+
+class Ex1_5 {
+    public static void main(String[] args) {
+	System.out.println(( 9.5 * 4.5 - 2.5 * 3 )/( 45.5 - 3.5));
+    }
+}
