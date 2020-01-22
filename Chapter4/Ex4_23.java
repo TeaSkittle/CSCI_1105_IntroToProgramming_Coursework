43925b64bceb0eca04815a3a1e9c441b4acdb00f
404kevnotfound <student@HSiMac4-9.local>
2020-01-22 09:31:49 -0700

    Fix Ex4_23

--- Chapter4/Ex4_23.java
+++ Chapter4/Ex4_23.java
@@ -9,33 +9,39 @@ import java.util.Scanner;
 
 class Ex4_23 {
     public static void main(String[] args) {
-	// Declare variables
-	Scanner input = new Scanner(System.in);
-	//String name;
-	double gross, federal, state, net;
+        // Declare variables
+        Scanner input = new Scanner(System.in);
+        //String name;
+        double gross, federal, state, totalDeduct, net;
 
-	// Get user input
-	System.out.print("Enter employee's name: ");
-	String name = input.next();
-	System.out.print("Enter number of hours worked: ");
-	double hours = input.nextDouble();
-	System.out.print("Enter hourly pay rate: ");
-	double rate = input.nextDouble();
+        // Get user input
+        System.out.print("Enter employee's name: ");
+        String name = input.next();
+        System.out.print("Enter number of hours worked: ");
+        double hours = input.nextDouble();
+        System.out.print("Enter hourly pay rate: ");
+        double rate = input.nextDouble();
+        System.out.print("Enter federal tax withholding rate: ");
+        double federalInput = input.nextDouble();
+        System.out.print("Enter state tax withholding: ");
+        double stateInput = input.nextDouble();
 
-	// Logic
-	gross = hours * rate;
-	federal = gross * 0.2;
-	state = gross * 0.09;
-	net = gross - (federal + state);
+        // Logic
+        gross = hours * rate;
+        federal = gross * federalInput;
+        state = gross * stateInput;
+        totalDeduct = federal + state;
+        net = gross - totalDeduct;
 
-	// Output
-	System.out.println("Employee's Name: " + name);
-	System.out.println("Hours Worked: " + hours);
-	System.out.println("Pay Rate: " + rate);
-	System.out.println("Gross Pay: " + gross);
-	System.out.println("Deductions: ");
-	System.out.printf("\tFederal Withholding: $%.02f\n", federal);
-	System.out.printf("\tState Withholding: $%.02f\n", state);
-	System.out.printf("Net Pay: $%.02f\n", net);
+        // Output
+        System.out.println("Employee's Name: " + name);
+        System.out.println("Hours Worked: " + hours);
+        System.out.println("Pay Rate: " + rate);
+        System.out.println("Gross Pay: " + gross);
+        System.out.println("Deductions: ");
+        System.out.printf("\tFederal Withholding: $%.02f\n", federal);
+        System.out.printf("\tState Withholding: $%.02f\n", state);
+        System.out.printf("\tTotal Deduction: $%.02f\n", totalDeduct);
+        System.out.printf("Net Pay: $%.02f\n", net);
     }
 }
