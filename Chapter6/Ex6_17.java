b936db98585be10f51b52a386f103e0f86768e42
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-01-30 10:20:47 -0700

    Update Ex6_17.java

--- Chapter6/Ex6_17.java
+++ Chapter6/Ex6_17.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 
 class Ex6_17 {
 	// Main method
-	public static void main(String[] args) {
+	public static void main( String[] args ) {
 		// Get user input
 		Scanner input = new Scanner( System.in );
-		System.out.print("Enter n: ");
+		System.out.print( "Enter n: " );
 		int n = input.nextInt();
 		
 		// Call method
@@ -21,19 +21,19 @@ class Ex6_17 {
 	// printMatrix method
 	public static void printMatrix( int n ) {
 		int col = 0;
-		for ( int row = 0; row <= n; row++) {
+		for ( int row = 0; row <= n; row++ ) {
 			if ( row < n ) {
-				// Print out random 0 or 1
-				System.out.print(( Math.round(Math.random() )));
+				// Print out a random 0 or 1
+				System.out.print((Math.round(Math.random())) + " "));
 			} else if ( row == n ) {
 				col++;
 				//print 0 or 1
 				System.out.printf("\n");
 				if ( col != n ) {
-					// Print out random 0 or 1
-					System.out.print(( Math.round(Math.random() )));
+					// Print out a random 0 or 1
+					System.out.print((Math.round(Math.random())) + " "));
 					row = 0;              // reset row count
-				} else if ( col >= n) {
+				} else if ( col >= n ) {
 					break;                // end loop
 				}
 			}
