33bed2632367f8b369991a5ac18bc9402ee0e009
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-02-10 11:39:59 -0700

    Update Ex6_31.java with javadocs comments.

--- Chapter6/Ex6_31.java
+++ Chapter6/Ex6_31.java
@@ -1,46 +1,55 @@
-/*
-Credit card numbers follow certain patterns. A credit card number must have between 13 and 16 digits. It must start with:
-  ■	4 for Visa cards 
-  ■	5 for Master cards 
-  ■	37 for American Express cards 
-  ■	6 for Discover cards
-In 1954, Hans Luhn of IBM proposed an algorithm for validating credit card numbers. The algorithm is useful to determine whether a card number is entered correctly or whether a credit card is scanned correctly by a scanner. Credit card numbers are generated following this validity check, commonly known as the Luhn check or the Mod 10 check, which can be described as follows (for illustra- tion, consider the card number 4388576018402626):
-
-1. Double every second digit from right to left. If doubling of a digit results in a two-digit number, add up the two digits to get a single-digit number.
-2. Now add all single-digit numbers from Step 1. 4 + 4 + 8 + 2 + 3 + 1 + 7 + 8 = 37
-3. Add all digits in the odd places from right to left in the card number. 6 + 6 + 0 + 8 + 0 + 7 + 8 + 3 = 38
-4. Sum the results from Step 2 and Step 3. 37 + 38 = 75
-5.	If the result from Step 4 is divisible by 10, the card number is valid; otherwise, it is invalid. For example, the number 4388576018402626 is invalid, but the number 438857601840263 is valid.
-*/
-package Ex6_31;
+package ex6_31;
 import java.util.Scanner;
-
+/**
+ * <p> This class checks the validity of credit card numbers using the Luhn check (also known as: Mod 10 check). </p>
+ * <p> Created: 2/10/2020 </p>
+ * @author Travis Dowd
+ *
+ */
 public class Ex6_31 {
+	/**
+	 * Main method, get credit card number from user and call isValid method.
+	 * @param args Command line arguments, not used in this class
+	 */
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    
     System.out.print("Enter a credit card number as a long integer: ");
     long number = input.nextLong();
-    
     if (isValid(number))
       System.out.println(number + " is valid");
     else
       System.out.println(number + " is invalid"); 
   }
-
-  /** Return true if the card number is valid */
+  /**
+   * <p>Checks the validity of the credit card number by calling these methods:</p>
+   * <ul>
+   * <li>getSize( number )</li>
+   * <li>prefixMatched( number )</li>
+   * <li>sumOfDoubleEvenPlace( number )</li>
+   * <li>sumOfOddPlace( number )</li>
+   * </ul>
+   * @param number (long; Credit card number)
+   * @return true if the card number is valid
+   */
   public static boolean isValid(long number) {
     return  (getSize(number) >= 13) && (getSize(number) <= 16) && 
         (prefixMatched(number, 4) || prefixMatched(number, 5) ||
         prefixMatched(number, 6) || prefixMatched(number, 37)) && 
        (sumOfDoubleEvenPlace(number) + sumOfOddPlace(number)) % 10 == 0;
   }
-
-  /** Get the result from Step 2 */
+  /**
+   * <p>1. Double every second digit from right to left. If doubling of a digit results in a two-digit number, add up the two digits to get a single-digit number.
+   * <br>2. Now add all single-digit numbers from Step 1
+   * Example: <br>
+   * 4388576018410707<br>
+   * <i><b>0 + 0 + 8 + 2 + 3 + 1 + 7 + 8 = 29</i></b><br>
+   * @param number (long; Credit card number)
+   * @return The sum of every other digit from right to left
+   */
   public static int sumOfDoubleEvenPlace(long number) {
     int result = 0;
     
-    number = number / 10; // Starting from the second digit from left
+    number = number / 10; // Starting from the second digit from right
     while (number != 0) {
       result += getDigit((int)((number % 10) * 2));
       number = number / 100; // Move to the next even place
@@ -49,8 +58,11 @@ public class Ex6_31 {
     return result;
   }
   
-  /** Return this number if it is a single digit, otherwise, return 
-   * the sum of the two digits */
+  /**
+   * If number if a single digit return the number, else add the two digits together and return the sum.
+   * @param number (int; number to be added with its own digits)
+   * @return Return number if it is a single digit, otherwise, return the sum of the two digits.
+   */
   public static int getDigit(int number) {
     return number % 10 + (number / 10);
   }
