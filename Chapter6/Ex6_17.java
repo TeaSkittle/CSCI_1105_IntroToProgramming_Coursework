27fd807cca3bd297b58a258dd2450cbb8acce4d6
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-02-10 09:59:23 -0700

    Update Ex6_17.java with javadocs comments

--- Chapter6/Ex6_17.java
+++ Chapter6/Ex6_17.java
@@ -1,42 +1,47 @@
-/*
-Author: Travis Dowd
-Date: 1-30-2020
-
-Chapter 6, exercise 6-17
-*/
-
+package ex6_17;
 import java.util.Scanner;
-
-class Ex6_17 {
-	// Main method
+/**
+ * <h1> Exercise 6-17 </h1>
+ * <p> Display a matrix of 0s and 1s, example: </p>
+ * <p> Enter n: 3
+ * <br> 0 1 0 
+ * <br> 1 0 0 
+ * <br> 0 0 1 </p>
+ * <p> Created: 2/10/2020 </p>
+ * @author Travis Dowd
+ * @version 2.0
+ */
+public class Ex6_17 {
+	/**
+	 * Main method, used to get user input and call the printMatrix method.
+	 * @param args Command line arguments, not used in this class
+	 */
 	public static void main( String[] args ) {
-		// Get user input
 		Scanner input = new Scanner( System.in );
 		System.out.print( "Enter n: " );
 		int n = input.nextInt();
-		
-		// Call method
 		printMatrix( n );
 	}
-	// printMatrix method
+	/**
+	 * Prints an n-by-n matrix of random 0s and 1s.
+	 * @param n (int; the x and y values of the matrix to be printed)
+	 */
 	public static void printMatrix( int n ) {
 		int col = 0;
 		for ( int row = 0; row <= n; row++ ) {
 			if ( row < n ) {
-				// Print out a random 0 or 1
-				System.out.print((Math.round(Math.random())) + " "));
+				System.out.print((Math.round(Math.random())) + " ");
 			} else if ( row == n ) {
 				col++;
-				//print 0 or 1
 				System.out.printf("\n");
 				if ( col != n ) {
-					// Print out a random 0 or 1
-					System.out.print((Math.round(Math.random())) + " "));
-					row = 0;              // reset row count
+					System.out.print((Math.round(Math.random())) + " ");
+					row = 0;
 				} else if ( col >= n ) {
-					break;                // end loop
+					break; 
 				}
 			}
 		}
 	}
 }
+
