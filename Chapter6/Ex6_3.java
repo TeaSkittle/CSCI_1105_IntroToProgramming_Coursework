de618aefab425e07544bc44b0cb4deb9368b59d2
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-02-10 10:26:10 -0700

    Update Ex6_3.java with javadocs comments

--- Chapter6/Ex6_3.java
+++ Chapter6/Ex6_3.java
@@ -1,26 +1,34 @@
-/*
-Author: Travis Dowd
-Date: 1-31-2020
-
-Chapter 6, exercise 6-3
-*/
-
+package ex6_3;
 import java.util.Scanner;
-
-class Ex6_3 {
+/**
+ * <h1> Exercise 6-3 </h1>
+ * <p> See if a an integer is a palindrome, example: </p>
+ * <p> Enter an integer: 12321
+ * <br> Is a palindrome. </p>
+ * @author Travis Dowd
+ * @version 2.0
+ *
+ */
+public class Ex6_3 {
+	/**
+	 * Main method, get user input and call isPalindrome method.
+	 * @param args Command line arguments, not used in this class
+	 */
 	public static void main( String[] args ) {
-		// Get user input
 		Scanner input = new Scanner( System.in );
 		System.out.print( "Enter an integer: " );
 		int number = input.nextInt();
-		
-		// Call methods
-		reverse( number );                         // Reverse the number inputed by user
-		isPalindrome( number );                    // Check to see if number == reverse( number )
+		isPalindrome( number );                // Check to see if number == reverse( number )
 	}
-	
+	/**
+	 * <p> Takes an integer and reverses the digits, example: </p>
+	 * <p> number = 123
+	 * <br>reversed number = 321 </p>
+	 * @param number (int; Number to be reversed)
+	 * @return Number with digits in the reverse order
+	 */
 	public static int reverse( int number ) {
-		int digit, rev_int;		           // Declare variables for the loop below
+		int digit, rev_int;		               // Declare variables for the loop below
 		rev_int = 0;
 		while ( number != 0 ) {
 			digit = number % 10;               // Get last digit of number
@@ -29,9 +37,16 @@ class Ex6_3 {
 		}
 		return rev_int;
 	}
-
+	/**
+	 * <p> Checks to see if number is equal to reverse( number ). </p>
+	 * <p> Example: <br>
+	 * 242 == 242 ( returns true) <br>
+	 * 123 == 321 ( returns false) </p>
+	 * @param number (int; The number to be reversed and checked)
+	 * @return True if number is equal to reverse( number ), else returns false.
+	 */
 	public static boolean isPalindrome( int number ) {
-		if ( number == reverse(number) ) {         // Compare inputed number with reversed number
+		if ( number == reverse(number) ) {        // Compare inputed number with reversed number
 			System.out.println("Is a palindrome.");
 			return true;
 		} else {
