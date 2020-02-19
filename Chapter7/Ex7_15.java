fe01f801da5b98d8fc0fec99615778da0c256b89
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-02-19 09:53:44 -0700

    Update Ex7_15.java

--- Chapter7/Ex7_15.java
+++ Chapter7/Ex7_15.java
@@ -1,5 +1,5 @@
 // Author: Travis Dowd
-// Date: 2-18-2020
+// Date: 2-19-2020
 // Chapter 7, exercise 7-15
 
 import java.util.Scanner;
@@ -7,31 +7,29 @@ import java.util.Arrays;
 
 class Ex7_15 {
 	public static void main( String[] args ) {
-		Scanner input = new Scanner( System.in );
-		int[] array = new int[ 10 ] ;                                        // Set new array with size of 10
+		Scanner input = new Scanner( System.in );                            // Create new inpute instance
+		int[] array = new int[ 10 ];                                         // Set new array with size of 10
 		System.out.print( "Enter 10 numbers seperated by a space: ");        // Prompt for user input
-		for (int i = 0; i < array.length; i++)                               // Set up loop using array.length
-			array[ i ] = input.nextInt();                                    // Loop for input 10 times
-		eliminateDuplicates( array );
+		for (int i = 0; i < array.length; i++) {                             // Set up loop using array.length
+			array[ i ] = input.nextInt();                                // Loop for input 10 times
+		} eliminateDuplicates( array );                                      // Call eleminateDuplicate method
 	}
-	public static int eliminateDuplicates( int[] list ) {
-		int distinct = 0;                                                    // Number of distinct numbers
+	public static int eliminateDuplicates( int[] list ) {                        // Method takes an aray as an argument
+		int j = 0;                                                           // Distinct numbers
 		Arrays.parallelSort( list );                                         // Sort array
 		int[] newList = new int[ list.length ];                              // New array with size of list
 		for ( int i = 0; i < list.length - 1; i++ ) {                        // Start of loop to create new array
-			if ( list[ i ] != list[i + 1] ) {  
-				++distinct;
-				newList[ i ] = list[ i ];
-			} else {
-				newList[ i ] = list[ i ];
-				++i;
+			if (list[ i ] != list[ i+1 ]){                               // Compare list element with next element in list
+				newList[j++] = list[i];                              // Assign and skip element
 			}
-		} Arrays.parallelSort( newList );
-		System.out.print("\nThe number of distinct numbers is: " + distinct + "\n");
-		System.out.print("Array without duplicates: ");
-		for (int j = 0; j < newList.length; j++ ) {
-			System.out.print(newList[j]);
-		}
-		return newList[0];
+		} newList[j++] = list[list.length-1];                                // Get last element of array
+		int[] listThree = new int[ j ];                                      // Create new array without trailing 0's
+		for (int i = 0; i < j; i++){                                         // For loop to create final array
+			listThree[i] = newList[i];                                   // Assign each element until j his hit
+		} System.out.print("\nThe number of distinct numbers is: " + j);     // int j is used for distinct variables
+		System.out.print("\nArray without duplicates: ");                    
+		for (int i = 0; i < listThree.length; i++ ) {                        // Print off array wihtout duplicates
+			System.out.print(listThree[ i ] + " ");                      // Have spaces between each element of array
+		} return listThree[0];                                               // return final list
 	}
 }
