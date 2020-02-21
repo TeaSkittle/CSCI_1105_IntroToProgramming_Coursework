974aeeba857d5bfdfa72d368af1314076971c508
Travis Dowd <dowd.tra8546@stu.stech.edu>
2020-02-21 09:51:04 -0700

    Update Ex7_23.java

--- Chapter7/Ex7_23.java
+++ Chapter7/Ex7_23.java
@@ -1,16 +1,15 @@
 // Author: Travis Dowd
-// Date: 2-19-2020
+// Date: 2-21-2020
 // Chapter 7, exercise 7-23
 
 class Ex7_23 {
 	public static void main( String[] args ) {
-		int[] lockers = new int[ 100 ];
-		int[] student = new int[ 100 ];
-		for ( int i = 0; i < student.length; i++ ) {
-			for ( int j = 0; j < lockers.length; j++ ) {
-				if ( i == 0 || j == 0 ) { 
-					if ( student[ i ] == 0 ) { lockers[ i ] = 1; } 
-				} else if ( i % j == 0 ) {
+		int[] lockers = new int[ 101 ];
+		int[] student = new int[ 101 ];
+		for ( int i = 1; i < student.length; ++i ) {
+			for ( int j = 1; j < lockers.length; ++j ) {
+				System.out.println(i + " " + j); 
+				if ( i % j == 0 ) {
 					if ( lockers[ i ] == 0 ) {
 						lockers[ i ] = 1;
 					} else {
@@ -19,7 +18,7 @@ class Ex7_23 {
 				}
 			}
 		}
-		for ( int i = 0; i < lockers.length; i++ ) {
+		for ( int i = 1; i < lockers.length; ++i ) {
 			if ( i % 10 == 0 ) { System.out.print("\n"); }
 			else
 				System.out.print( lockers[i] + " " );
