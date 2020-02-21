5a809258029b67430112fb8eb0f7f9c51c9730a9
Travis Dowd <dowd.tra8546@stu.stech.edu>
2020-02-21 09:57:18 -0700

    Update Ex7_23.java

--- Chapter7/Ex7_23.java
+++ Chapter7/Ex7_23.java
@@ -4,24 +4,22 @@
 
 class Ex7_23 {
 	public static void main( String[] args ) {
-		int[] lockers = new int[ 101 ];
-		int[] student = new int[ 101 ];
-		for ( int i = 1; i < student.length; ++i ) {
-			for ( int j = 1; j < lockers.length; ++j ) {
-				System.out.println(i + " " + j); 
-				if ( i % j == 0 ) {
-					if ( lockers[ i ] == 0 ) {
-						lockers[ i ] = 1;
-					} else {
-						lockers[ i ] = 0;
+		int[] lockers = new int[ 101 ];                    // Array for lockers
+		int[] student = new int[ 101 ];                    // Array ofor students
+		for ( int i = 1; i < student.length; ++i ) {       // iterate through students
+			for ( int j = 1; j < lockers.length; ++j ) {   // iterate through lockers
+				if ( i % j == 0 ) {                        // Figure out which lockers to skip, via remainder == 0
+					if ( lockers[ i ] == 0 ) {             // If locker is closed:
+						lockers[ i ] = 1;                  // Open the locekr
+					} else {                               // Else:
+						lockers[ i ] = 0;                  // Close the locker
 					}
 				}
 			}
 		}
-		for ( int i = 1; i < lockers.length; ++i ) {
-			if ( i % 10 == 0 ) { System.out.print("\n"); }
-			else
-				System.out.print( lockers[i] + " " );
+		for ( int i = 1; i < lockers.length; ++i ) {       // iterate through lockers once more
+			System.out.print( lockers[i] + " " );          // Print status of locker
+			if ( i % 10 == 0 ) { System.out.print("\n"); } // Print newline on 10
 		}
 	}
 }
