4a0b8ecd3902c02ce30160dc3ae3a53a6e9afad6
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-03-04 11:38:17 -0700

    Update Mod5Exam.java

--- Chapter8/Mod5Exam.java
+++ Chapter8/Mod5Exam.java
@@ -26,14 +26,14 @@ class Mod5ExamPart1 {
 			trackAtt();
 		}
 	}
-	public static void printExam() {
+	public static void printExam() {					// Method to print exam scores
 		Scanner input  = new Scanner( System.in );			// Create new Scanner
-		double[] scores;						// Array to stoe exam scores
+		double[] scores;						// Array to store exam scores
 		scores = new double[25];					// Set size of array to 25 (number of students)
 		int a = 0, b = 0, c = 0, d = 0, f = 0;				// Initialize grades
-		double sum = 0, average = 0;					// Decalre average and sum, used for calculating average
+		double sum = 0, average = 0;					// Declare average and sum, used for calculating avrage
 		System.out.print( "\nEnter 25 exam scores: " );			// Prompt user for input
-		for ( int i = 0; i < 25; i++ ) {				// Begin loopoing through scores[] for exam grade
+		for ( int i = 0; i < 25; i++ ) {				// Begin looping through scores[] for exam scores
 			scores[i] = input.nextDouble();				// Get input for scores[i]
 			sum += scores[i];					// Add scores[i] to sum (for average)
 			if ( scores[i] > 90.0 && scores[i] < 100.0 ) { a++; }	// Number of A's
@@ -42,20 +42,20 @@ class Mod5ExamPart1 {
 			if ( scores[i] > 60.0 && scores[i] < 70.0  ) { d++; }	// Number of D's
 			if ( scores[i] < 60.0 && scores[i] > 0.0   ) { f++; }	// Number of F's
 		} average = sum / scores.length;				// Divide sum by size of array to get average
-		System.out.printf( "\nAverage score: %.2f%%\n", average );	// Print out average
+		System.out.printf( "\nAverage score: %.2f%%\n", average );	// Print average
 		System.out.printf( "Number of A's: %d\n", a );			// Print A's
 		System.out.printf( "Number of B's: %d\n", b );			// Print B's
 		System.out.printf( "Number of C's: %d\n", c );			// Print C's
 		System.out.printf( "Number of D's: %d\n", d );			// Print D's
 		System.out.printf( "Number of F's: %d\n", f );			// Print F's
 	}
-	public static void trackAtt() {
-		Scanner input  = new Scanner(System.in);
-		int att[][] = new int[5][5];
-		int days = 0;	
-		System.out.print("\nHow many days would you like to enter? ");
-		days = input.nextInt();	
-		
+	public static void trackAtt() {						// Method to track attendance of students
+		Scanner input  = new Scanner(System.in);                        // Create new Scanner
+		int att[][] = new int[5][5];					// Create a 5x5 array for classroom seating
+		int days = 0;							// Start days at zero
+		System.out.print("\nHow many days would you like to enter? ");  // Prompt user for input
+		days = input.nextInt();						// Get number of days to track attendance for
+										// Print diagram of classroom
 		System.out.println("This is the diagram of your seating chart. Enter attendance to match this chart.\n");
 		System.out.println("	   Front of Room");
 		System.out.println("|--------------------------------|");
@@ -68,16 +68,16 @@ class Mod5ExamPart1 {
 		
 		for (int d = 0; d < days; d++) {				// Loop through each day
 			System.out.println("\nDay " + (d+1));			// Print out day before input
-			for (int r = 0; r < att.length; r++) {			// Loop though each row
+			for (int r = 0; r < att.length; r++) {			// Loop through each row
 				System.out.print("Enter 5 numbers for row "		
-				+ r + " (1 = absent, 0 = present): ");		// Prompt for input
-				for (int c = 0; c < att[r].length; c++) {	// Loop though each column
+				+ r + " (1 = absent, 0 = present): ");		// Prompt user for input
+				for (int c = 0; c < att[r].length; c++) {	// Loop through each column
 					att[r][c] = input.nextInt() + att[r][c];// Increment attendance if missed
 				}
 			}
 		}  // Display seating chart showing number of absences;
-		System.out.println("\nStudent Attendance: ");			// Begin print of attendance
-		for (int r = 0; r < att.length; r++) {				// Loop though each row
+		System.out.println("\nStudent Attendance: ");			// Begin printing of attendance
+		for (int r = 0; r < att.length; r++) {				// Loop through each row
 			System.out.print( "\n" );				// Print new line
 			for (int c = 0; c < att[r].length; c++) {		// Loop through each column
 				System.out.print( att[r][c] + " " );		// Print out attendance
