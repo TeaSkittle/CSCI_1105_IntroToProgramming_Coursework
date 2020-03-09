7d0c378ae752bd238f201971a09a4a67927bebc4
404kevnotfound <student@HSiMac4-9.local>
2020-03-09 11:31:31 -0600

    Update ChordED.java

--- Final/ChordED.java
+++ Final/ChordED.java
@@ -11,55 +11,73 @@ import java.io.*;
 class ChordED {
 	public static void main( String[] args ) throws IOException {
 		Scanner input = new Scanner( System.in );
-		int[] finger = new int[6];
 		String name = "";
 		String[][] guitar = { 
-			{ "-", "-", "-", "-" }, { "-", "-", "-", "-" }, { "-", "-", "-", "-" },
-			{ "-", "-", "-", "-" }, { "-", "-", "-", "-" }, { "-", "-", "-", "-" }
-		};
-		
+			{ "-", "-", "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-", "-", "-" },
+			{ "-", "-", "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-", "-", "-" },
+			{ "-", "-", "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-", "-", "-" }
+		};	
 		System.out.print( "Enter name of chord: " );
 		name = input.nextLine();
-		
-		getInput( guitar, finger, name );
-		printChord( guitar, finger, name );
-		writeFile( guitar, finger, name );
+		getInput( guitar, name );
+		printChord( guitar, name );
+		writeFile( guitar, name );
 	}
-	public static void getInput( String[][] chord, int[] fing_pos, String title ) {
+	public static void getInput( String[][] chord, String title ) {
 		Scanner input = new Scanner( System.in );
 		for ( int i = 0; i < 6; i++ ) {
 			int j = i + 1;
-			System.out.print( "Enter fret to played on string " + j + ": " );
+			switch (j) {
+				case 1:
+					System.out.print( "Enter fret to played on high E: " );
+					break;
+				case 2:
+					System.out.print( "Enter fret to played on B: " );
+					break;
+				case 3:
+					System.out.print( "Enter fret to played on high G: " );
+					break;
+				case 4:
+					System.out.print( "Enter fret to played on D: " );
+					break;	
+				case 5:
+					System.out.print( "Enter fret  to played on high A: " );
+					break;
+				case 6:
+					System.out.print( "Enter fret to played on low E: " );
+					break;							
+			}
 			int fret = input.nextInt();
-			chord[i][fret] = "O";
+			String strFret = Integer.toString( fret );
+			chord[i][fret] = strFret;
 		}
 	}
-	public static void printChord( String[][] chord, int[] fing_pos, String title ) {
+	public static void printChord( String[][] chord, String title ) {
 		System.out.printf( "\t%s\n", title );
-		System.out.println( "   -------------" );
+		System.out.println( "   -----------------" );
 		for ( int i = 0; i < 6; i++ ) {
 			System.out.print( chord[i][0] + " || " );
-			for ( int j = 1; j < 4; j++ ) {
-				System.out.print( chord[i][j] + " | " );
-				if ( j == 3 ) { 
-					System.out.print( fing_pos[i] + "\n" );
+			for ( int j = 1; j < 8; j++ ) {
+				System.out.print( chord[i][j] + " " );
+				if ( j == 7 ) { 
+					System.out.print( "||\n" );
 				}
 			}
-		} System.out.println ("   -------------" );
+		} System.out.println ("   -----------------" );
 	}
-	public static void writeFile(String[][] chord, int[] fing_pos, String title ) throws IOException {
+	public static void writeFile(String[][] chord, String title ) throws IOException {
 		try (FileWriter fileWriter = new FileWriter( "lib/" + title + ".txt")) {
 			fileWriter.write( "\t" + title + "\n" );
-			fileWriter.write( "   -------------\n" );
+			fileWriter.write( "   -----------------\n" );
 			for ( int i = 0; i < 6; i++ ) {
 				fileWriter.write( chord[i][0] + " || " );
-				for ( int j = 1; j < 4; j++ ) {
-					fileWriter.write( chord[i][j] + " | " );
-					if ( j == 3 ) { 
-						fileWriter.write( fing_pos[i] + "\n" );
+				for ( int j = 1; j < 8; j++ ) {
+					fileWriter.write( chord[i][j] + " " );
+					if ( j == 7 ) { 
+						fileWriter.write( "||\n" );
 					}
 				}
-			} fileWriter.write ("   -------------" );
+			} fileWriter.write ("   -----------------" );
 		}
 	}
 }
