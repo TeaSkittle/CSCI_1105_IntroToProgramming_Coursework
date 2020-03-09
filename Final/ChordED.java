104ccb6e5f41ac06c7c26749b681b977944c4d3d
404kevnotfound <student@HSiMac4-9.local>
2020-03-09 10:59:10 -0600

    Added lib folder

--- Final/ChordED.java
+++ Final/ChordED.java
@@ -6,22 +6,27 @@
 // Intro to Programming: Final Project
 
 import java.util.Scanner;
+import java.io.*;
 
 class ChordED {
-	public static void main( String[] args ) {
+	public static void main( String[] args ) throws IOException {
+		Scanner input = new Scanner( System.in );
 		int[] finger = new int[6];
 		String name = "";
 		String[][] guitar = { 
 			{ "-", "-", "-", "-" }, { "-", "-", "-", "-" }, { "-", "-", "-", "-" },
 			{ "-", "-", "-", "-" }, { "-", "-", "-", "-" }, { "-", "-", "-", "-" }
 		};
+		
+		System.out.print( "Enter name of chord: " );
+		name = input.nextLine();
+		
 		getInput( guitar, finger, name );
 		printChord( guitar, finger, name );
+		writeFile( guitar, finger, name );
 	}
 	public static void getInput( String[][] chord, int[] fing_pos, String title ) {
 		Scanner input = new Scanner( System.in );
-		System.out.print( "Enter name of chord: " );
-		title = input.nextLine();
 		for ( int i = 0; i < 6; i++ ) {
 			int j = i + 1;
 			System.out.print( "Enter fret to played on string " + j + ": " );
@@ -41,5 +46,20 @@ class ChordED {
 				}
 			}
 		} System.out.println ("   -------------" );
+	}
+	public static void writeFile(String[][] chord, int[] fing_pos, String title ) throws IOException {
+		try (FileWriter fileWriter = new FileWriter( "lib/" + title + ".txt")) {
+			fileWriter.write( "\t" + title + "\n" );
+			fileWriter.write( "   -------------\n" );
+			for ( int i = 0; i < 6; i++ ) {
+				fileWriter.write( chord[i][0] + " || " );
+				for ( int j = 1; j < 4; j++ ) {
+					fileWriter.write( chord[i][j] + " | " );
+					if ( j == 3 ) { 
+						fileWriter.write( fing_pos[i] + "\n" );
+					}
+				}
+			} fileWriter.write ("   -------------" );
+		}
 	}
 }
