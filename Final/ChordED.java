3c0ce09b237a54a2c528212c2d91a33da3e30f22
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-03-11 14:38:22 -0600

    Update ChordED.java

--- Final/ChordED.java
+++ Final/ChordED.java
@@ -113,14 +113,14 @@ class ChordED {
 	public static void listChords() {
 		try {
 			Runtime run = Runtime.getRuntime();						// Create new Runtime
-			Process ps  = run.exec("./printChords.sh");					// Create process set to ./printChords.sh
+			Process ps  = run.exec( "./printChords.sh" );					// Create process set to ./printChords.sh
 			ps.waitFor();									// Wait until process is finished
-			BufferedReader buff = new BufferedReader(new InputStreamReader(ps.getInputStream()));	// Create new buffer for reading shell script
+			BufferedReader buff = new BufferedReader( new InputStreamReader(ps.getInputStream()) );	// Create new buffer for reading shell script
 			String line = "";								// Empty String
-			while ((line = buff.readLine()) != null) {					// While line in proccess is not NULL
-				System.out.println(line);						// Print out line
+			while ( (line = buff.readLine()) != null ) {					// While line in proccess is not NULL
+				System.out.println( line );						// Print out line
 			} buff.close();									// Close buffer
-		} catch (Exception ex) {								// If process fails
+		} catch ( Exception ex ) {								// If process fails
 			ex.printStackTrace();								// Print stack trace
 		}
 	}
@@ -133,18 +133,18 @@ class ChordED {
 		Scanner input = new Scanner( System.in );						// Create new Scanner
 		String name = "";									// Empty string name for chord title
 		if ( args[0].equals("-h") ) { 								// Display help
-			System.out.printf("Command line options: \n");
-			System.out.printf("\t -h\t Show help\n");
-			System.out.printf("\t -l\t List saved chords\n");
-			System.out.printf("\t -n\t Create new chord. Follow '-n' with name of chord, for example: \n");
-			System.out.printf("\t\t\t $ java ChordED -n Em\n");
+			System.out.printf( "Command line options: \n" );
+			System.out.printf( "\t -h\t Show help\n" );
+			System.out.printf( "\t -l\t List saved chords\n" );
+			System.out.printf( "\t -n\t Create new chord. Follow '-n' with name of chord, for example: \n" );
+			System.out.printf( "\t\t\t $ java ChordED -n Em\n" );
 		} else if ( args[0].equals("-l") ) {							// List saved chords
-			listChords();
+			listChords();									// Call listChords() method
 		} else if ( args[0].equals("-n") ) {							// create new chord
 			name = args[1];									// Set name = args[1]
-			getInput( guitar, name );							// Get input for chord
+			getInput(   guitar, name );							// Get input for chord
 			printChord( guitar, name );							// Print out chord inputted
-			writeFile( guitar, name );							// Save chord to .txt file
+			writeFile(  guitar, name );							// Save chord to .txt file
 		}
 	}
 }
